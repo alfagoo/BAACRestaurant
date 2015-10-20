e2dc8d60e83d1a147ab5beccc1da8a5d1a90b51e
kib <tassamon.so@baac.or.th>
2015-10-20 09:57:44 +0700

    delete all SQLite

--- app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
+++ app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.baac.baacrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,9 +20,19 @@ public class MainActivity extends AppCompatActivity {
         creteAndConnceted(); //create Method
 
         //Tester Add New Value
-        testerAdd();
+        //testerAdd();
+
+        deleteAllSQLite();
 
     }//Main Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("BAAC.db", MODE_PRIVATE, null);
+        objSqLiteDatabase.delete("userTABLE", null, null);
+        objSqLiteDatabase.delete("foodTABLE", null, null);
+
+    }
 
     private  void testerAdd() {
 
